4ec9ff73745489fc0e6686205d55d1ffed0c20cd
alierrahman <alierrahman@gmail.com>
2015-05-30 22:09:58 +0700

    PIPOLONDO

--- simplemath/src/balok/hitung.java
+++ simplemath/src/balok/hitung.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class hitung {
 		public int menu(){
 			Scanner masuk = new Scanner(System.in);
-			int pil;
-			System.out.println("====== MENU ======");
+			int pil=0;
+			System.out.println("====== MENU ===============");
 			System.out.println("1.Hitung Luas Balok");
 			System.out.println("2.Hitung Volume Balok");
 			System.out.println("3.Hitung Keliling Balok");
 			System.out.println("4.Hitung Diagonal Balok");
 			System.out.println("5.Hitung Luas Segitiga");
+			System.out.println("6.Hitung PIPOLONDO");
+			System.out.println("===========================");
 			System.out.print("Masukkan Pilihan anda : ");
 			pil=masuk.nextInt();
 			if (pil==1){
@@ -19,6 +21,8 @@ public class hitung {
 				System.out.print(obyek.masuk());
 				int p=0,l=0,t=0;
 				System.out.print(obyek.luasBalok(p,l,t));
+				}else if(pil==6){
+					pipolondo();
 				}
 			return pil;
 		}
@@ -40,6 +44,40 @@ public class hitung {
 			int luasBalok;
 			luasBalok=2*p*l+2*p*t+2*l*t;
 			return luasBalok;
+		}
+		public void pipolondo(){
+			Scanner masuk=new Scanner(System.in);
+			int a,b, hasil;
+			String pilihan;			
+			
+			System.out.print("Masukan pipolondo / (x,/,+,-) = ");
+			pilihan=masuk.nextLine();
+			System.out.print("Masukan nilai A:");
+			a=masuk.nextInt();
+			System.out.print("Masukan nilai B:");
+			b=masuk.nextInt();
+
+			if(pilihan.equals("x")||pilihan.equals("X")){
+				hasil=a*b;
+				System.out.print("Masukan nilai B:"+hasil);
+			}else if(pilihan.equals(":")){
+		        hasil = a/b;
+		        System.out.print("Hasil dari A:B="+hasil);
+		      
+		      }else if(pilihan.equals("+")){
+		        hasil = a+b;
+		        System.out.print("Hasil dari A+B="+hasil);
+		      
+		      }else if(pilihan.equals("-")){
+		        hasil = a-b;
+		        System.out.print("Hasil dari A-B="+hasil);
+		      }else{
+				
+			}
+			System.out.print("\n==========================");
+			System.out.print("\n");
+			
+			
 		}
 		
 		
